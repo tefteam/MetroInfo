e89fbc5a31bd46aefbc8068e84e8e7f4b834fca6
andy <razzer9876@gmail.com>
2016-05-09 17:33:47 +0300

    CLass for Place item

--- app/src/main/java/com/example/andy/metroinfo/dto/PlacesDTO.java
+++ app/src/main/java/com/example/andy/metroinfo/dto/PlacesDTO.java
@@ -4,4 +4,27 @@ package com.example.andy.metroinfo.dto;
  * Created by andy on 09.05.16.
  */
 public class PlacesDTO {
+    private int info;
+    private int info_exit;
+
+    public PlacesDTO(int info, int info_exit) {
+        this.info = info;
+        this.info_exit = info_exit;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public int getInfo_exit() {
+        return info_exit;
+    }
+
+    public void setInfo_exit(int info_exit) {
+        this.info_exit = info_exit;
+    }
 }
