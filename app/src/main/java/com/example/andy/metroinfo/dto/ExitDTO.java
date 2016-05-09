7338b613546f950692baf4ff15e5a156a555ed52
andy <razzer9876@gmail.com>
2016-05-09 17:33:14 +0300

    Class for Exit item

--- app/src/main/java/com/example/andy/metroinfo/dto/ExitDTO.java
+++ app/src/main/java/com/example/andy/metroinfo/dto/ExitDTO.java
@@ -4,4 +4,27 @@ package com.example.andy.metroinfo.dto;
  * Created by andy on 09.05.16.
  */
 public class ExitDTO {
+    private int exit_ind;
+    private int exit;
+
+    public ExitDTO(int exit_ind, int exit) {
+        this.exit_ind = exit_ind;
+        this.exit = exit;
+    }
+
+    public int getExit_ind() {
+        return exit_ind;
+    }
+
+    public void setExit_ind(int exit_ind) {
+        this.exit_ind = exit_ind;
+    }
+
+    public int getExit() {
+        return exit;
+    }
+
+    public void setExit(int exit) {
+        this.exit = exit;
+    }
 }
